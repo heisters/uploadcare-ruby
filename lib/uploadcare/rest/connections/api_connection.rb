e6b8a9fd173d5930deab529ff3695d10473f73c4
Ian Heisters <ian@heisters.co>
2016-11-03 12:31:53 -0700

    Update api_connection.rb

--- lib/uploadcare/rest/connections/api_connection.rb
+++ lib/uploadcare/rest/connections/api_connection.rb
@@ -7,13 +7,13 @@ module Uploadcare
         super options[:api_url_base] do |frd|
           frd.request :url_encoded
           frd.use ::FaradayMiddleware::FollowRedirects, limit: 3
-          frd.adapter :net_http # actually, default adapter, just to be clear
           frd.headers['Authorization'] = "Uploadcare.Simple #{options[:public_key]}:#{options[:private_key]}"
           frd.headers['Accept'] = "application/vnd.uploadcare-v#{options[:api_version]}+json"
           frd.headers['User-Agent'] = Uploadcare::user_agent
 
           frd.response :raise_error
           frd.response :parse_json
+          frd.adapter Faraday.default_adapter # actually, default adapter, just to be clear
         end
       end
     end
